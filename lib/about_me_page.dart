08fb0e7d45a72e85a49ba12fb095078df7767c66
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-04-17 13:30:11 +0200

    added single child scroll

--- lib/about_me_page.dart
+++ lib/about_me_page.dart
@@ -20,44 +20,46 @@ class AboutMePage extends StatelessWidget {
               margin: const EdgeInsets.all(16),
               child: Padding(
                 padding: const EdgeInsets.all(20),
-                child: Column(
-                  spacing: 4,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-            
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Container(
-                        width: 200,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 95, 93, 93), 
-                            width: 4.0,
+                child: SingleChildScrollView(
+                  child: Column(
+                    spacing: 4,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                              
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: const Color.fromARGB(255, 95, 93, 93), 
+                              width: 4.0,
+                            ),
+                          ),
+                          child: const CircleAvatar(
+                            radius: 150,
+                            backgroundImage: AssetImage('assets/img/ceol_and_shokri.JPG'), 
                           ),
                         ),
-                        child: const CircleAvatar(
-                          radius: 150,
-                          backgroundImage: AssetImage('assets/img/ceol_and_shokri.JPG'), 
-                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
+                      const SizedBox(height: 20),
+                    
                   
-
-                    const Text("Shokri Francis Raoof",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                    const Text("Future Flutter Dev in Training",
-                    style: TextStyle(fontSize: 12),),
-                      SizedBox(height: 8,),
-                    const Text(
-                      'I am an irish guy living in Berlin for the past 10 years now learning how to code in Dart/Flutter. Currently developing an app to help people learn music theory and also a Pokédex. I also have a little 1 year old mini dachshund named Ceol("Music" in Irish)',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
+                      const Text("Shokri Francis Raoof",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                      const Text("Future Flutter Dev in Training",
+                      style: TextStyle(fontSize: 12),),
+                        SizedBox(height: 8,),
+                      const Text(
+                        'I am an irish guy living in Berlin for the past 10 years now learning how to code in Dart/Flutter. Currently developing an app to help people learn music theory and also a Pokédex. I also have a little 1 year old mini dachshund named Ceol("Music" in Irish) ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
